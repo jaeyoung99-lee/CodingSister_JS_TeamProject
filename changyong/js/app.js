88f8b07276b0a89cd2fad1720586bdf8a11f1802
ghd075 <ghd075@gmail.com>
2024-07-17 03:12:42 +0900

    [feat] 240717 current Category-based place search view - Display address information for map center coordinates on screen - Update map center address information and marker position - Implement category-based place search feature (limited to 15 results per search due to free version)

--- changyong/js/app.js
+++
@@ -1,8 +0,0 @@
-import config from './config.js';
-
-const kakaoMapApiKey = config.kakaoMapApiKey;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const kakaoMapScript = document.getElementById('kakaoMapScript');
-  kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoMapApiKey}&libraries=services`;
-});
