8aedda60656cf660c22b482c0d3302a2191b841a
Hyunjun Lee <leehyunjune11@gmail.com>
2024-07-21 18:41:47 +0900

    [css]240721 css update

--- js/weather.js
+++ js/weather.js
@@ -1,30 +1,13 @@
-//weather
 const weatherApiKey = config.weatherapiKey;
 
-const getWeather = async (city) => {
-  const encodedCity = encodeURIComponent(city);
-  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodedCity}&units=metric&lang=kr&appid=${weatherApiKey}`;
-  try {
-    const response = await fetch(weatherApiUrl);
-    if (!response.ok) {
-      throw new Error("날씨 정보를 가져오는 중에 오류가 발생했습니다.");
-    }
-    const data = await response.json();
-    displayWeather(data);
-    console.log(data);
-  } catch (error) {
-    console.error("날씨 정보를 가져오는 동안 문제가 발생했습니다. 문제 내용은 다음과 같습니다. ", error.message);
-  }
-};
-
 // 날씨 정보를 화면에 표시하는 함수
-const displayWeather = (data) => {
-  const weatherDiv = document.getElementById("weather");
+const displayWeather = (data, elementId) => {
+  const weatherLocation = document.getElementById(elementId);
   const weatherDescription = data.weather[0].description;
   const temperature = data.main.temp;
   const city = data.name;
 
-  weatherDiv.innerHTML = `
+  weatherLocation.innerHTML = `
     <p>도시 : ${city}</p>
     <p>날씨 : ${weatherDescription}</p>
     <p>온도 : ${temperature}℃</p>
@@ -45,7 +28,7 @@ const locationWeather = () => {
           throw new Error("현재 위치의 날씨 정보를 가져오는 중에 오류가 발생했습니다.");
         }
         const data = await response.json();
-        displayWeather(data);
+        displayWeather(data, "weather");
         console.log(data);
       } catch (error) {
         console.error("현재 위치의 날씨 정보를 가져오는 동안 문제가 발생했습니다. 문제 내용은 다음과 같습니다. ", error.message);
@@ -58,7 +41,25 @@ const locationWeather = () => {
   }
 };
 
+// 사용자가 입력한 도시 이름으로 날씨 정보를 가져오는 함수
+const getWeather = async (city) => {
+  const encodedCity = encodeURIComponent(city);
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodedCity}&units=metric&lang=kr&appid=${weatherApiKey}`;
+  try {
+    const response = await fetch(weatherApiUrl);
+    if (!response.ok) {
+      throw new Error("날씨 정보를 가져오는 중에 오류가 발생했습니다.");
+    }
+    const data = await response.json();
+    displayWeather(data, "weatherBox");
+    console.log(data);
+  } catch (error) {
+    console.error("날씨 정보를 가져오는 동안 문제가 발생했습니다. 문제 내용은 다음과 같습니다. ", error.message);
+  }
+};
+
 // 페이지 로드 시 현재 위치의 날씨 정보를 가져옴
+window.addEventListener("load", locationWeather);
 
 // 사용자가 입력한 도시 이름으로 날씨 정보를 가져오는 이벤트 리스너
 document.getElementById("search-form").addEventListener("submit", (event) => {
